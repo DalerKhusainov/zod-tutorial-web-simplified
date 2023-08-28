7381d1c6bc39560a06842efcd2b27cd74791d168
DalerKhusainov <dalerkhusainov85@gmail.com>
2023-08-28 13:56:32 +0500

    created the template form validation with zod and react hook form

--- zod-tutorial/src/App.tsx
+++ zod-tutorial/src/App.tsx
@@ -1,41 +1,86 @@
 import "./App.css";
-
 // ZOD HOOKS
-import { z } from "zod";
+import { z, ZodType } from "zod";
+// REACT HOOK FROM
+import { useForm } from "react-hook-form";
+//
+import { zodResolver } from "@hookform/resolvers/zod";
 
-// PUTTING HOBBIES VALUE IN AN ARRAY
-const hobbies = ["Programming", "Weight Lifting", "Guitar"] as const;
-
-hobbies.push("Gaming"); // it says Property 'push' does not exist on type 'readonly ["Programming", "Weight Lifting", "Guitar"]'.
-
-// CREATING ZOD SCHEMA
-const UserSchema = z.object({
-  userName: z.string().min(3).max(5),
-  age: z.number().gt(12),
-  birthday: z.date(),
-  isProgrammer: z.boolean(),
-  hobbies: z.enum(hobbies), // <== WE GET no ERROR HERE
-});
-
-type User = z.infer<typeof UserSchema>;
-
-const user: User = {
-  userName: "WDS",
-  age: 13,
-  birthday: new Date(),
-  isProgrammer: false,
-  hobbies: "Programming",
+type FormData = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  age: number;
+  password: string;
+  confirmPassword: string;
 };
 
-console.log(UserSchema.safeParse(user).success); // true
-
 function App() {
-  // const [count, setCount] = useState(0)
+  //
+  const schema: ZodType<FormData> = z
+    .object({
+      firstName: z.string().min(2).max(30),
+      lastName: z.string().min(2).max(30),
+      email: z.string().email(),
+      age: z.number().min(18).max(70),
+      password: z.string().min(5).max(20),
+      confirmPassword: z.string().min(5).max(20),
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+      message: "Passwords do not match",
+      path: ["confirmPassword"],
+    });
+
+  //
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
+
+  //
+  const submitData = (data: FormData) => {
+    console.log("It worked", data);
+  };
 
   return (
     <>
       <div>
-        <h1>Zod Titorial</h1>
+        <form onSubmit={handleSubmit(submitData)}>
+          <h3>Register</h3>
+          <label>First Name:</label>
+          <input type="text" {...register("firstName")} />
+          {errors.firstName && <span>{errors.firstName.message}</span>}
+
+          <label>Last Name:</label>
+          <input type="text" {...register("lastName")} />
+          {errors.lastName && <span>{errors.lastName.message}</span>}
+
+          <label>Email:</label>
+          <input type="email" {...register("email")} />
+          {errors.email && <span>{errors.email.message}</span>}
+
+          <label>Age:</label>
+          <input type="number" {...register("age", { valueAsNumber: true })} />
+          {errors.age && <span>{errors.age.message}</span>}
+
+          <label>Password:</label>
+          <input type="password" {...register("password")} />
+          {errors.password && <span>{errors.password.message}</span>}
+
+          <label>Confirm Password:</label>
+          <input type="password" {...register("confirmPassword")} />
+          {errors.confirmPassword && (
+            <span>{errors.confirmPassword.message}</span>
+          )}
+
+          <input type="submit" className="submit-btn" />
+          {/* <button className="submit-btn" type="button">
+            Submit
+          </button> */}
+        </form>
       </div>
     </>
   );
